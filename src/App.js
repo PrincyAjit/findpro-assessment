4b1eee43694c15e9ad0fcc9f05f9414e7a2c7fd5
princy <princyajit898@gmail.com>
2023-03-17 15:54:01 +0530

    Added default path in routes.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Navigate to="/" replace={true} />} />
-        <Route path="/" element={<FaqDashboard />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace={true} />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace={true} />} />
+        <Route path="/dashboard" element={<FaqDashboard />} />
         <Route path="/contactus" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
